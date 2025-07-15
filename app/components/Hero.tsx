269ea6571e4266adc136356ec5c427c8a04e5728
Vipin Das <lokeshbs@radiants.com>
2025-07-15 22:20:28 +0530

    asssets mvoed

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero() {
           playsInline
           className="w-full h-full object-cover"
         >
-          <source src={'/assets/hero.mp4'} type="video/mp4" />
+          <source src={'/hero.mp4'} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         {/* Gradient Overlay */}
