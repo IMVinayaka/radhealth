b04db531ae0fca8ff46adef1bac1579d02c469ab
Vipin Das <lokeshbs@radiants.com>
2025-07-16 20:35:05 +0530

    z index increased

--- app/open-jobs/page.tsx
+++ app/open-jobs/page.tsx
@@ -374,7 +374,7 @@ export default function CareersPage() {
 
       </div>
       {/* Job Application Modal */}
-      <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+      <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'>
 
 
       <JobApplicationModal
